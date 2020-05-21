15fd8954bf52e48e2fa244918b1b2965fdeed996
o0pmitev <pmitev89@gmail.com>
2020-05-21 12:53:17 +0200

    Updates

---
+++ precache-manifest.3a4834d71b039c5508f188e1de681eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7f31000ceaf08b455fe892278f6ca2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "da43e89cd02df1203c3a",
+    "url": "/my-app/static/css/main.fd3e81b6.chunk.css"
+  },
+  {
+    "revision": "7801bfc80a3313b373ea",
+    "url": "/my-app/static/js/2.2407abc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2407abc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da43e89cd02df1203c3a",
+    "url": "/my-app/static/js/main.9b185ccd.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  }
+]);
